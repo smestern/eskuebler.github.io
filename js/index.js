6635f72857f013122e3bc501f9c82c78b1fa8514
KartikP <kartikspradeepan@gmail.com>
2020-01-07 15:13:43 -0500

    edit

--- js/index.js
+++ js/index.js
@@ -69,6 +69,15 @@ document
     animated: false,
     mode: 'particle',
     dom_element: document.getElementById('container'),
+	colors:[
+    0x00be9e,
+    0x3919cb,
+    0x7d0bc4,
+    0xff6700,
+    0x3eef00,
+    0xffce00,
+    0xf50027,
+    0x606060],
     metadata: mdata,
     showAxes: 10000,
     cameraChangeCallback: (data) => { console.log(data) }
